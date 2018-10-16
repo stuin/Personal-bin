00b6893d88504a834ab899db0c838eb2422a214e
Stuart <stuin01@gmail.com>
2018-10-15 19:18:51 -0700

    Added comments and rearranged steam script

--- steam-create.sh
+++ steam-create.sh
@@ -1,28 +1,25 @@
+#Get data from steamdb
 id=$1
 raw=$(curl -L https://steamdb.info/app/$id | grep -o "<title>[^·]*\|<img class=[^>]*")
-echo $raw
 
+#Separate out title and image
 title=$(echo $raw | cut -d '<' -f2 | sed 's/title>//')
 image=$(echo $raw | cut -d '<' -f3 | awk '{print $4}' | sed 's/src=\"//' | sed 's/\"//')
 
 echo "Setting up $title"
 
-base="[Desktop Entry]\n
-Name=$title\n
-Comment=Play this game on Steam\n
-Exec=steam steam://rungameid/$id\n
-Icon=steam_icon_$id\n
-Terminal=false\n
-Type=Application\n
-Categories=Game;\n"
-
+#Define base file paths
+icon="$HOME/.local/share/icons/hicolor/32x32/apps/steam_icon_$id"
 file="$HOME/.local/share/applications/$title.desktop"
 
+#Define shortcut file contents
+base="[Desktop Entry] \nName=$title \nComment=Play this game on Steam \nExec=steam steam://rungameid/$id \nIcon=steam_icon_$id \nTerminal=false \nType=Application \nCategories=Game;\n"
+
+#Create actual shortcut
 echo -en $base > "$file"
 chmod +x "$file"
 
-icon="$HOME/.local/share/icons/hicolor/32x32/apps/steam_icon_$id"
-
+#download and format image
 curl -o "$icon.jpg" $image
 convert "$icon.jpg" "$icon.png"
 rm "$icon.jpg"
